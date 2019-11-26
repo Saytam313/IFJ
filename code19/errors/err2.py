afe69ec65beed5fb352439aa78babcae1c77004f
bahno <34132745+bahno@users.noreply.github.com>
2019-11-26 21:20:57 +0100

    Nalezené chyby u while a if

---
+++ code19/errors/err2.py
@@ -0,0 +1,13 @@
+a=7	
+while a > 5:
+	a=a-1
+	print('Num: ',a,'\n')
+	if a == 6:
+		print('NUM JE 6ka','\n')	
+print('done')
+#koment	
+
+
+#	error řádek 8 - komentář
+#	2 STATEMENT error type: 2
+#	solo komentář na posledním řádku problém??
---
+++ code19/errors/err3.py
@@ -0,0 +1,13 @@
+a=7	
+while a > 2:
+	a=a-1
+	print('Num: ',a,)
+	if a == 6:
+		print('NUM JE 6ka')
+	else:
+		a == 2
+print('done')
+
+
+# 2 STATEMENT error type: 2
+# python přeloží v pořádku, ale naše dává error
